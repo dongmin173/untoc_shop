adb54fe80541f16e11207aeb4f700aa79b582865
dongmin173 <ehdals3046@naver.com>
2024-11-15 17:42:06 +0900

    11월 15일

---
+++ item/item_router.py
@@ -0,0 +1,95 @@
+from fastapi import APIRouter, HTTPException, Depends, Response,Security
+
+from sqlalchemy.orm import Session
+from database import get_itemdb
+
+from .item_schema import Create_item, Modify_item, Item
+from models import Item as Item_model
+
+router = APIRouter(
+    prefix="/item"
+)
+
+
+def insert_data(db, table):
+    db.add(table)
+    db.commit()
+    db.refresh(table)
+
+@router.get("/")
+@router.get("/get_items")
+def get_items(skip:int = 0, limit:int = 10,
+              item_db : Session = Depends(get_itemdb)):
+    item = item_db.query(Item_model).all()
+    return item[skip : skip + limit]
+
+@router.get("/get_item")
+def get_item(item_id:int,
+             item_db: Session = Depends(get_itemdb)):
+    item = item_db.query(Item_model).filter(Item_model.item_id == item_id).first()
+    if not item:
+        raise HTTPException(status_code=404, detail=f"item_id : {item_id} is None.")
+    
+    return item
+
+@router.post("/create_item", response_model=Create_item)
+def create_item(item:Create_item, 
+                item_db: Session = Depends(get_itemdb)):
+    
+    create_items = Item_model(
+                item_name=item.item_name, 
+                item_price=item.item_price,
+                amount=item.amount,
+                create_at=item.create_at,
+                create_date=item.create_date)
+
+    insert_data(item_db, create_items)
+
+    return create_items
+
+@router.put("/update_item", response_model=Modify_item)
+def update_item(item: Modify_item,
+                item_id: int,
+                item_db: Session = Depends(get_itemdb)):
+    
+    modify_item = Modify_item(item_name=item.item_name,
+                              item_price=item.item_price,
+                              amount=item.amount)
+
+    item = item_db.query(Item_model).filter(Item_model.item_id == item_id).first()
+
+    """
+    item_db에서 Item_model에 해당하는 table을 들고온 후 Item_model.item_id와 입력한 item_id가 같은 첫번째 row를 들고온다.
+
+    이때 Item_model은 model.py에 작성한 DB table의 양식이다.
+
+    이 형식은 자주 등장할 것이니 기억해주세요!
+
+    """
+
+    if not item:
+        raise HTTPException(status_code=404, detail=f"item_id : {item_id} is None.")
+
+    """
+    item이 있는지 확인하여 없으면 예외처리한다.
+
+    """
+
+    item.item_name = modify_item.item_name
+    item.item_price = modify_item.item_price
+    item.amount = modify_item.amount
+
+    item_db.commit()
+    item_db.refresh(item)
+
+    return modify_item
+
+@router.delete("/delete_item/{item_id}")
+def delete_item(item_id: int,
+            item_db: Session = Depends(get_itemdb)):
+    item = item_db.query(Item_model).filter(Item_model.item_id == item_id)
+    if not item.first():
+        raise HTTPException(status_code=404, detail=f"item_id : {item_id} is None.")
+    item.delete()
+    item_db.commit()
+    return {"message":f"item_id : {item_id} - success delete"}
---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from item.item_router import router as item_router
+
+from database import item_Base, item_engine
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+item_Base.metadata.create_all(bind=item_engine)
+
+app.include_router(item_router, tags=["item"])
+
+#uvicorn main:app --reload
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
